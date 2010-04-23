02738235217e7bc06dc590bb2ab8a8a4b868a98e
Ondrej Certik <ondrej@certik.cz>
2010-04-22 17:46:08 -0700

    Removing obsolete code

--- examples/inline.py
+++ examples/inline.py
@@ -19,6 +19,34 @@ class TestClass(object):
         alert(self.value)
         self.value += 1
 
+class AjaxHelper(object):
+    def call(self, method, uri, query=''):
+        callback = {
+                'success':self.subSuccess,
+                'failure':self.subFailure,
+                'argument':[self]
+            }
+
+        if query != '':
+            args = []
+            for key in query:
+                args[args.length] = key + '=' + encodeURIComponent(query[key])
+            query = args.join('&')
+
+        YAHOO.util.Connect.asyncRequest(method, uri, callback, query)
+
+    def get(self, uri, query=None):
+        self.call('GET', uri, query)
+
+    def post(self, uri, query=None):
+        self.call('POST', uri, query)
+
+    def subSuccess(self, o):
+        o.argument[0].success(YAHOO.lang.JSON.parse(o.responseText))
+
+    def subFailure(self, o):
+        o.argument[0].failure(o)
+
 @JavaScript
 class AjaxTest(AjaxHelper):
     def __init__(self):
