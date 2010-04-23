062173504f0fa670e2a578f58defaf7fa65fad12
Ondrej Certik <ondrej@certik.cz>
2010-04-22 18:33:52 -0700

    Better tests

--- tests/test_run_js.py
+++ tests/test_run_js.py
@@ -2,19 +2,26 @@ import os
 
 from py2js import JavaScript
 
-def f(x):
+def f1(x):
     return x
+
+def f2(x):
+    return x**2
 
 def test(func, run):
     func_source = str(JavaScript(func))
     run_file = "/tmp/run.js"
+    defs = open("defs.js").read()
     with open(run_file, "w") as f:
+        f.write(defs)
+        f.write("\n")
         f.write(func_source)
         f.write("\n")
         f.write(run)
-    r = os.system('js -f defs.js -f %s' % run_file)
+    r = os.system('js -f %s' % run_file)
     assert r == 0
 
 
-test(f, "assert(f(3) == 3)")
-test(f, "assert(f(3) != 4)")
+test(f1, "assert(f1(3) == 3)")
+test(f1, "assert(f1(3) != 4)")
+test(f2, "assert(f2(3) == 9)")
