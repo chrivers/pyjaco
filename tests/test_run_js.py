0fa1ea31d9f9bd1c1433f6d49478fcbdf42de377
Ondrej Certik <ondrej@certik.cz>
2010-04-22 18:43:16 -0700

    More tests

--- tests/test_run_js.py
+++ tests/test_run_js.py
@@ -1,6 +1,7 @@
 import os
 
-from test_compile_js import f1, f2, f3
+from test_compile_js import (f1, f2, f3, f3b, f3c, f3d, f3e, f4, f5, ifs1,
+        ifs2, ifs3, ifs4, loop1)
 
 def test(func, run):
     run_file = "/tmp/run.js"
@@ -15,7 +16,20 @@ def test(func, run):
     assert r == 0
 
 
-test(f1, "assert(f1(3) == 3)")
-test(f1, "assert(f1(3) != 4)")
-test(f2, "assert(f2(3) == 8)")
-test(f3, "assert(f3(3) == -1)")
+test(f1, "assert(f1(3) == 3);")
+test(f1, "assert(f1(3) != 4);")
+test(f2, "assert(f2(3) == 8);")
+test(f3, "assert(f3(3) == -1);")
+test(f3b, "assert(f3b(3) == -1);")
+#test(f3c, "assert(f3c(3) == -1);")
+test(f3d, "assert(f3d(3) == 20);")
+test(f3e, "assert(f3e(3) == 9);")
+test(f4, "assert(f4(true) == 5);")
+test(f4, "assert(f4(false) == 6);")
+test(f5, "assert(f5(true) == 2);")
+test(f5, "assert(f5(false) == 0);")
+
+test(ifs1, "ifs1(1);")
+test(ifs2, "ifs2(1);")
+test(ifs3, "ifs3(1);")
+test(ifs4, "ifs4(1);")
