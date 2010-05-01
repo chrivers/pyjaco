ad1706a65d8d00075e44ad6d3708c6bf1092249b
Ondrej Certik <ondrej@certik.cz>
2010-04-30 18:57:30 -0700

    Expand the str.replace() tests

--- tests/strings/replace.py
+++ tests/strings/replace.py
@@ -5,3 +5,10 @@ txt2 = txt.replace("the","a")
 
 print txt
 print txt2
+
+print txt.replace("the", "a", 0)
+print txt.replace("the", "a", 1)
+print txt.replace("the", "a", 2)
+print txt.replace("the", "a", 3)
+print txt.replace("the", "a", 4)
+print txt.replace("the", "a", 50)
