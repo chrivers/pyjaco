f3266b2dd3b79e6365286ce0fe3eee7766111dfe
Ondrej Certik <ondrej@certik.cz>
2010-05-06 22:43:08 -0700

    Improve kwargs test

--- tests/basic/kwargs.py
+++ tests/basic/kwargs.py
@@ -1,12 +1,14 @@
+def myfunc(a, b, *c, **d):
+    print a
+    print b
+    for i in c:
+        print i
+    keys = d.keys()
+    keys.sort()
+    for i in keys:
+        print i
+        print d[i]
 
-def myfunc(a,b,*c,**d):
-	print a
-	print b
-	for i in c:
-		print i
-	for i in d:
-		print i
-		print d[i]
-
-myfunc(1,2,bar='a',foo='c')
-
+myfunc(1, 2, bar='a', foo='c')
+print
+myfunc(1, 2, 3, 4, bar='a', foo='c')
