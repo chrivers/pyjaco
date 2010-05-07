31d5f88d085623227d0fa3f9a1193055ee6ec9e5
Ondrej Certik <ondrej@certik.cz>
2010-05-06 22:52:16 -0700

    $m added from PJs

--- py-builtins.js
+++ py-builtins.js
@@ -1195,3 +1195,184 @@ _str.prototype.lower = function() {
 _str.prototype.upper = function() {
     return str(this._obj.toUpperCase());
 };
+
+/**
+This function is taken from PJs.
+
+  PJs is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  PJs is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with PJs.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2010 Jared Forsyth <jared@jareforsyth.com>
+
+ * How to use:
+
+    $def([defaults], [aflag], [kflag], fn);
+
+    defaults, aflag, and kflag are all optional, but required to be in that
+        order to avoid ambiguity.
+
+    defaults = an associative array of key, value pairs; the key is the arg
+        name, anf the vaule is default value.
+
+    aflag signals that the last (or second-to-last, if kflag is true) is to be
+        populated with excess positional arguments. (in python, this is the *args
+        syntax).
+
+    kflag is like aflag, but for positional arguments, e.g. **kwargs.
+
+    there's also checks happening the whole way, so you won't be stuck debugging
+    another annoying undefined error.
+
+    Here's an example that uses all of these:
+
+    var foo = $def({c:null, d:10}, true, true, function foo(a, b, c, d, args, kwargs) {
+        // only a and b are required, and excess positional and dictionary
+        // arguments will be captured.
+        console.log([a, b, c, d, args, kwargs]);
+    });
+
+    and in use...
+
+    > foo(1);
+    TypeError: foo requires 2 arguments (1 given)
+    > foo(1,2);
+    [1, 2, null, 10, [], {}]
+    > foo(1,2,3);
+    [1, 2, 3, 10, [], {}]
+    > foo(1,2,3,4,5,6,7,8,9);
+    [1, 2, 3, 4, [5, 6, 7, 8, 9], {}]
+
+    now some some real magic; dictionary arguments:
+
+    > foo.args([1], {'b':9, 'd':20, 'man':'hatten'}
+    [1, 9, null, 20, [], {'man': 'hatten'}]
+
+    !! that looks like python !! well...almost. but it's lovely :)
+**/
+
+var to_array = function(a){return Array.prototype.slice.call(a,0);};
+var fnrx = /function\s+\w*\s*\(([\w,\s]*)\)/;
+
+String.prototype.strip = function(){
+    return this.replace(/^\s+/,'').replace(/\s+$/,'');
+};
+
+function $def() {
+    var args = to_array(arguments);
+    if (!args.length) {
+        throw "$def requires at least one argument...";
+    }
+    var fn = args.pop();
+    if (typeof(fn)!=='function')
+        throw "ParseError: $def requires a function as the last argument";
+
+    var match = (fn+'').match(fnrx);
+    if (!match)
+        throw "ParseError: sorry, something went wrong on my end; are you sure you're passing me a valid function?" + (fn+'');
+    fn.__args__ = match[1].split(',');
+    if (fn.__args__.length == 1 && !fn.__args__[0].strip())
+        fn.__args__ = [];
+    for (var i=0;i<fn.__args__.length;i++){
+        fn.__args__[i] = fn.__args__[i].strip();
+    }
+    if (fn.__args__.length != fn.length)
+        throw "ParseError: sorry, something went wrong on my end; are you sure you're passing me a valid function? (arg nums didn't line up "+fn.__args__+' '+fn.length+")" + fn;
+
+    var defaults = args.length?args.shift():{};
+    if (defaults === false) { // no args checking...? what?
+        return fn;
+    }
+    var fargs = args.length?args.shift():false;
+    var fkwargs = args.length?args.shift():false;
+
+    if (args.length)
+        throw "$def takes a max of 4 arguments";
+
+    var argnum = fn.__args__.length;
+    if (fargs) argnum-=1;
+    if (fkwargs) argnum-=1;
+
+    var dflag = false;
+    for (var i=0;i<argnum;i++) {
+        if (defined(defaults[fn.__args__[i]])) dflag = true;
+        else if (dflag) {
+            throw "SyntaxError in function " + fn.name + ": non-default argument follows default argument";
+        }
+    }
+    var ndefaults = 0;
+    for (var x in defaults) ndefaults++;
+
+    var meta = function() {
+        var args = to_array(arguments);
+        var catchall = [];
+        var catchdct = {};
+        if (args.length > argnum) {
+            if (fargs) {
+                catchall = args.slice(argnum);
+                args = args.slice(0, argnum);
+            } else
+                throw "TypeError: " + fn.name + "() takes "+argnum+" arguments (" + args.length + " given)";
+        } else {
+            for (var i=args.length;i<argnum; i++){
+                if (!defined(defaults[fn.__args__[i]])) {
+                    throw "TypeError: " + fn.name + "() takes at least " + (argnum-ndefaults) +" arguments (" + args.length + " given)";
+                }
+                args.push(defaults[fn.__args__[i]]);
+            }
+        }
+        if (fargs) args.push(catchall);
+        if (fkwargs) args.push(catchdct);
+        return fn.apply(null, args);
+    };
+    meta.args = function(pos, dict) {
+        var full = {};
+        for (var i=0;i<pos.length && i<argnum;i++) {
+            var name = fn.__args__[i];
+            full[name] = pos[i];
+        }
+        var catchall = pos.slice(i);
+        for (;i<argnum;i++) {
+            var name = fn.__args__[i];
+            if (defined(dict[name])) {
+                full[name] = dict[name];
+                delete dict[name];
+            } else if (defined(defaults[name])) {
+                full[name] = defaults[name];
+            } else
+                throw "TypeError: " + fn.name + " argument " + name + " was not satisfied.";
+        }
+        if (!fargs && catchall.length)
+            throw "TypeError: " + fn.name + "() takes "+argnum+" arguments (" + args.length + " given)";
+        if (!fkwargs && dict) {
+            for (var a in dict)
+                throw "TypeError: " + fn.name + "() got an unexpected keyword argument '" + a + "'";
+        }
+        var args = [];
+        for (var i=0;i<argnum;i++) {
+            args.push(full[fn.__args__[i]]);
+        }
+        if (fargs) args.push(catchall);
+        if (fkwargs) args.push(dict);
+        return fn.apply(null, args);
+    };
+    if (fn.__type__)
+        meta.__type__ = fn.__type__;
+    else
+        meta.__type__ = 'method';
+    meta.__wraps__ = fn;
+    meta.name = fn.name;
+    meta.__name__ = fn.name;
+    return meta;
+}
+
+/** end python function madness **/
