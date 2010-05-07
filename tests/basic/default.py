40e43ba242ab476324416522bac08faed9a669e3
Ondrej Certik <ondrej@certik.cz>
2010-05-06 23:27:04 -0700

    Reformat the default.py test

--- tests/basic/default.py
+++ tests/basic/default.py
@@ -1,12 +1,11 @@
-
 g = 99
 
-def f(a,b=1,c="default c",d=g):
+def f(a, b=1, c="default c", d=g):
 	print a
 	print b
 	print c
 	print d
 
 f(0)
-f(0,77)
-f(0,77,"hello")
+f(0, 77)
+f(0, 77, "hello")
