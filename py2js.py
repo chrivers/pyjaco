96ded5de43176a6fedc0a77742921ea544dc9ac9
Ondrej Certik <ondrej@certik.cz>
2010-10-04 22:01:19 -0700

    Improve the py2js string % handling

--- py2js.py
+++ py2js.py
@@ -570,8 +570,12 @@ class JS(object):
     def visit_BinOp(self, node):
         if isinstance(node.op, ast.Mod) and isinstance(node.left, ast.Str):
             left = self.visit(node.left)
-            right = self.visit(node.right)
-            return "vsprintf(js(%s), js(%s))" % (left, right)
+            if isinstance(node.right, (ast.Tuple, ast.List)):
+                right = self.visit(node.right)
+                return "vsprintf(js(%s), js(%s))" % (left, right)
+            else:
+                right = self.visit(node.right)
+                return "sprintf(js(%s), %s)" % (left, right)
         left = self.visit(node.left)
         right = self.visit(node.right)
 
