0141cc107c5f503a84585b117ce2fcdf801881b3
Samuel Ytterbrink <neppord@gmail.com>
2011-03-20 19:45:01 +0100

    matches almost all output of the old tester.

--- test.py
+++ test.py
@@ -1,46 +1,57 @@
 import unittest
 import os
 
-class Py2JsTestResult(unittest.runner.TextTestResult):
+class Py2JsTestResult(unittest.result.TestResult):
 
   def __init__(self, *a, **k):
-    unittest.runner.TextTestResult.__init__(self, *a, **k)
+    super(Py2JsTestResult, self).__init__(*a, **k)
     self.currentTest = None
-    self.state = "[OK]"
+    self.__state = "[OK]"
+    self.__faild = False
 
   def startTest(self, test):
+    super(Py2JsTestResult, self).startTest(test)
     if self.currentTest != test.templ["name"]:
       self.testsleft = 4
       self.currentTest = test.templ["name"]
       self.testsRun += 1
-      self.state = "[OK]"
-      self.row = self.currentTest
-    if self.state not in ["[OK]", "should fail but [OK]"]:
-      pass
-      #self.stop()
+      self.__state = "[OK]"
+      self.__faild = False
+      self.row = test.templ["py_path"].replace("\\","/") + " [4]: "
 
   def stopTest(self, test):
+    super(Py2JsTestResult, self).stopTest(test)
     self.testsleft -= 1
     if self.testsleft == 0:
-      print self.row + " " * (80 - len(self.row) - len(self.state)) + self.state
+      print self.row + " " * (80 - len(self.row) - len(self.__state)) + self.__state
 
   def addSuccess(self, test):
+    if self.__faild:
+      return
+    super(Py2JsTestResult, self).addSuccess(test)
     self.row += "."
-    self.state = "[OK]"
+    self.__state = "[OK]"
 
   def addUnexpectedSuccess(self, test):
-    self.unexpectedSuccesses += 1
+    if self.__faild:
+      return
+    super(Py2JsTestResult, self).addUnexpectedSuccess(test)
     self.row += "."
-    self.state = "should fail but [OK]",
+    self.__state = "should fail but [OK]"
 
   def addExpectedFailure(self, test, err):
-    self.expectedFailures += 1
-    self.row += "_"
-    self.state = "known to [FAIL]",
+    if self.__faild:
+      return
+    super(Py2JsTestResult, self).addExpectedFailure(test, err)
+    self.__faild = True
+    self.__state = "known to [FAIL]"
 
   def addFailure(self, test, err):
-    self.row += "_"
-    self.state = "[FAIL]"
+    if self.__faild:
+      return
+    super(Py2JsTestResult, self).addFailure(test, err)
+    self.__faild = True
+    self.__state = "[FAIL]"
 
 class Py2JsTestRunner(unittest.runner.TextTestRunner):
   resultclass = Py2JsTestResult
@@ -71,6 +82,41 @@ def compile_and_run_file_test(file_path, file_name=None):
         def test_c_run_javascript(self):
             cmd = 'js -f "%(js_path)s" > "%(js_out_path)s" 2> "%(error)s"' % self.templ
             self.assertEqual(0, os.system(cmd))
+        def test_d_check_output(self):
+            cmd = 'diff "%(py_out_path)s" "%(js_out_path)s" > "%(diff_path)s" 2> "%(error)s"' % self.templ
+            self.assertEqual(0, os.system(cmd))
+    CompileAndRunFile.__name__ = file_name
+    return CompileAndRunFile
+
+
+def compile_and_run_file_failing_test(file_path, file_name=None):
+    file_name = file_name if file_name else file_path
+
+    temp = {
+        "py_path": file_path, 
+        "py_out_path": file_path + ".out",
+        "js_path": file_path + ".js",
+        "js_out_path": file_path + ".js.out",
+        "diff_path": file_path + ".diff",
+        "error": file_path + ".err",
+        "name": file_name,
+        }
+
+    class CompileAndRunFile(unittest.TestCase):
+        templ = temp
+        @unittest.expectedFailure
+        def test_a_run_pyhton(self):
+            cmd = 'python "%(py_path)s" > "%(py_out_path)s" 2> "%(error)s"' % self.templ
+            self.assertEqual(0, os.system(cmd))
+        @unittest.expectedFailure
+        def test_b_compile_javascript(self):
+            cmd = 'python pyjs.py --include-builtins "%(py_path)s" > "%(js_path)s" 2> "%(error)s"' % self.templ
+            self.assertEqual(0, os.system(cmd))
+        @unittest.expectedFailure
+        def test_c_run_javascript(self):
+            cmd = 'js -f "%(js_path)s" > "%(js_out_path)s" 2> "%(error)s"' % self.templ
+            self.assertEqual(0, os.system(cmd))
+        @unittest.expectedFailure
         def test_d_check_output(self):
             cmd = 'diff "%(py_out_path)s" "%(js_out_path)s" > "%(diff_path)s" 2> "%(error)s"' % self.templ
             self.assertEqual(0, os.system(cmd))
--- tests.py
+++ tests.py
@@ -2,9 +2,71 @@ import test
 import unittest
 import glob
 import os
-    
+known_to_fail = [
+                "tests/basic/nestedclass.py",
+                "tests/basic/super.py",
+                "tests/basic/kwargs.py",
+                "tests/basic/float2int.py",
+                "tests/basic/oo_inherit.py",
+                "tests/basic/listcomp2.py",
+                "tests/basic/del_dict.py",
+                "tests/basic/del_local.py",
+                "tests/basic/sumcomp.py",
+                "tests/basic/del_array.py",
+                "tests/basic/valueerror.py",
+                "tests/basic/lambda.py",
+                "tests/basic/try.py",
+                "tests/basic/vargs.py",
+                "tests/basic/del_attr.py",
+                "tests/basic/del_global.py",
+                "tests/basic/del_slice.py",
+                "tests/basic/generator.py",
+                "tests/basic/raise.py",
+
+                "tests/functions/sort_cmp.py",
+                "tests/functions/ne.py",
+                "tests/functions/aug.py",
+                "tests/functions/floatdiv.py",
+                "tests/functions/sort23.py",
+
+                "tests/errors/decorator.py",
+
+                "tests/lists/filter.py",
+                "tests/lists/reduce.py",
+                "tests/lists/sum.py",
+                "tests/lists/subclass.py",
+
+                "tests/libraries/xmlwriter.py",
+
+                "tests/modules/classname.py",
+                "tests/modules/from_import.py",
+                "tests/modules/import.py",
+                "tests/modules/import_alias.py",
+                "tests/modules/import_class.py",
+                "tests/modules/import_diamond.py",
+                "tests/modules/import_global.py",
+                "tests/modules/import_multi.py",
+                "tests/modules/module_name.py",
+                "tests/modules/rng.py",
+
+                "tests/strings/string_format_d.py",
+                "tests/strings/string_format_efg.py",
+                "tests/strings/string_format_i.py",
+                "tests/strings/string_format_o.py",
+                "tests/strings/string_format_u.py",
+                "tests/strings/string_format_x.py",
+                "tests/strings/ulcase.py",
+                ]
+
 test_paths = glob.glob("tests/*/*.py")
-test_cases =[test.compile_and_run_file_test(test_path, os.path.basename(test_path)) for test_path in test_paths]
+test_paths.sort()
+test_cases = []
+for test_path in test_paths:
+    if test_path.replace("\\","/") not in known_to_fail:
+      test_cases.append(test.compile_and_run_file_test(test_path, os.path.basename(test_path)))
+    else:
+      test_cases.append(test.compile_and_run_file_failing_test(test_path, os.path.basename(test_path)))
+      
 
 def load_tests(loader, standard_tests, none):
     suite = unittest.TestSuite()
