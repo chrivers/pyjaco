40ccf867b295e31a1fed836771b0def9b4547d0c
Samuel Ytterbrink <neppord@gmail.com>
2011-05-09 03:19:44 +0200

    Added a -x option to ignore error output, and added error output.

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,10 @@
 #! python
+
 import optparse
 import testtools.runner
 import testtools.util
 import testtools.tests
+
 def main():
     option_parser = optparse.OptionParser(
         usage="%prog [options] [filenames]",
@@ -16,14 +18,31 @@ def main():
         default=False,
         help="run all tests (including the known-to-fail)"
         )
+    option_parser.add_option(
+        "-x",
+        "--no-error",
+        action="store_true",
+        dest="no_error",
+        default=False,
+        help="ignores error( don't display them after tests)"
+        )
     options, args = option_parser.parse_args()
     runner = testtools.runner.Py2JsTestRunner(verbosity=2)
+    results = None
     if options.run_all:
-        runner.run(testtools.tests.ALL)
+        results = runner.run(testtools.tests.ALL)
     elif args:
-        runner.run(testtools.tests.get_tests(args))
+        results = runner.run(testtools.tests.get_tests(args))
     else:
-        runner.run(testtools.tests.NOT_KNOWN_TO_FAIL)
+        results = runner.run(testtools.tests.NOT_KNOWN_TO_FAIL)
+    if not options.no_error and results.errors:
+        print
+        print "errors:"
+        print "  (use -x to skip this part)"
+        for test, error in results.errors:
+            print
+            print "*", str(test), "*"
+            print error
 
 if __name__ == "__main__":
   main()
