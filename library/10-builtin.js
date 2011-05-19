f58bfa21b896861945ab34a4854920fc8fa3e5fc
Christian Iversen <ci@sikkerhed.org>
2011-05-19 19:31:05 +0200

    + Implemented standard functions filter() and reduce(). Fixes 2 more test cases

--- library/10-builtin.js
+++ library/10-builtin.js
@@ -277,3 +277,35 @@ py_builtins.print = function(s) {
     }
 };
 
+py_builtins.filter = Function(function(f, l) {
+   res = list.__call__();
+   iterate(iter.__call__(l), function(item) {
+     if (py_builtins.bool(f(item))) {
+       res.append(item);
+     };
+   });
+   return res;
+});
+
+py_builtins.reduce = Function(function(func, seq) {
+    var initial;
+    if (arguments.length == 3) {
+        initial = arguments[2];
+    } else {
+        initial = null;
+    }
+    if (len(seq) < 2) {
+        return initial;
+    }
+    if (arguments.length == 3) {
+        var accum = initial;
+        var start = 0;
+    } else {
+        var accum = func(seq.__getitem__(0), seq.__getitem__(1));
+        var start = 2;
+    }
+    for (var i = start; i < len(seq); i++) {
+        accum = func(accum, seq.__getitem__(i));
+    }
+    return accum;
+});
