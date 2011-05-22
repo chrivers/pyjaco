b5a433e961e41db50acadecd4d7d85745d3a1e9e
Christian Iversen <ci@sikkerhed.org>
2011-05-23 01:18:12 +0200

    + Added test of basic comparisons

---
+++ tests/basic/comparison.py
@@ -0,0 +1,12 @@
+
+print 1 < 2
+print 2 > 1
+print 1 <= 1
+print 2 >= 2
+print 1 == 1
+print True and True
+print True or False
+print False or True
+print not False
+print not False and not False
+print not False or False
