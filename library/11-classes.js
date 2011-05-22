262071215898d62f3d0760843d81a0ce1eb91f96
Christian Iversen <ci@sikkerhed.org>
2011-05-23 01:59:52 +0200

    * Made object use __inherit() so it is completely consistent with the other classes

--- library/11-classes.js
+++ library/11-classes.js
@@ -29,36 +29,6 @@ var ObjectMetaClass = function(cls) {
     this.prototype = cls.prototype;
 };
 
-var object = function() {
-
-    var x = function() { /* Class constructor */ };
-
-    x.prototype.__init__ = function() {
-    };
-
-    x.prototype.__setattr__ = function(k, v) {
-        this[k] = v;
-    };
-
-    x.prototype.__getattr__ = function(k) {
-        return this[k];
-    };
-
-    x.prototype.__delattr__ = function(k) {
-        delete this[k];
-    };
-
-    x.prototype.__repr__ = function() {
-        return str.__call__("<instance of " + this.__class__.__name__ + ">");
-    };
-
-    x.prototype.toString = function() {
-        return js(this.__repr__());
-    };
-
-    return new ObjectMetaClass(x);
-}();
-
 var __inherit = function(cls, name) {
 
     if (!defined(name)) {
@@ -77,4 +47,44 @@ var __inherit = function(cls, name) {
     res.__name__ = name;
     res.prototype.__class__ = res;
     return res;
+};
+
+var object = __inherit(function() {}, "object");
+
+object.prototype.__init__ = function() {
+};
+
+object.prototype.__setattr__ = function(k, v) {
+    this[k] = v;
+};
+
+object.prototype.__getattr__ = function(k) {
+    return this[k];
+};
+
+object.prototype.__delattr__ = function(k) {
+    delete this[k];
+};
+
+object.prototype.__repr__ = function() {
+    return str.__call__("<instance of " + this.__class__.__name__ + ">");
+};
+
+object.prototype.__str__ = object.prototype.__repr__;
+
+object.prototype.__ne__ = function (other) {
+    return py_builtins.__not__(this.__eq__(other));
+};
+
+object.prototype.__cmp__ = function (y) {
+    var g = this.__gt__(y);
+    if (js(g)) {
+        return 1;
+    } else {
+        return -js(this.__lt__(y));
+    }
+};
+
+object.prototype.toString = function () {
+    return js(this.__str__());
 };
