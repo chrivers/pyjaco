f883b5bc63765c8bab83d611c31ed0472cf12fa9
Christian Iversen <ci@sikkerhed.org>
2011-05-26 09:42:47 +0200

    + Added test of common expressions

---
+++ tests/functions/expr.py
@@ -0,0 +1,16 @@
+from __future__ import division
+
+a = 244
+b = 23
+
+print a
+print a + 4
+print a - 2
+print a << 4
+print a >> 2
+print a | 234324
+print a & 213213
+print a ^ 2312
+print a // 324
+print a / 2
+print b ** 3
