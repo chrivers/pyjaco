cddcaceeb5c359e72da2100ea1f30c12afc561ca
Christian Iversen <ci@sikkerhed.org>
2011-06-01 16:11:20 +0200

    + Added initial version of a super-object to deal with super(). Not yet complete

---
+++ library/13-super.js
@@ -0,0 +1,45 @@
+/**
+  Copyright 2011 Christian Iversen <ci@sikkerhed.org>
+
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation
+  files (the "Software"), to deal in the Software without
+  restriction, including without limitation the rights to use,
+  copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following
+  conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
+**/
+
+var Super = __inherit(object, "Super");
+
+Super.prototype.__init__ = function(cls, obj) {
+    this.cls = cls;
+    this.obj = obj;
+};
+
+Super.prototype.__getattr__ = function(k) {
+    return this.cls.__super__.prototype[k];
+};
+
+Super.prototype.__repr__ = function() {
+    return str.__call__("<super " + this.cls.toString() + ", " + this.obj.toString() + ">");
+};
+
+Super.prototype.__str__ = Super.prototype.__repr__;
+
+var __super = Function(function(scls, obj) {
+    return Super.__call__(scls, obj);
+});
