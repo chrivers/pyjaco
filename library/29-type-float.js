02549c29b7e448873d3c483dfb6c680c89ec4d4d
Christian Iversen <ci@sikkerhed.org>
2011-11-14 16:26:14 +0100

    * Converted <float> to new __create__ api

--- library/29-type-float.js
+++ library/29-type-float.js
@@ -38,11 +38,11 @@ float.PY$__init__ = function(value) {
 
 var __float_real__ = float.PY$__create__;
 
-float.PY$__create__ = function(obj) {
+float.PY$__create__ = function(cls, obj) {
     if (js(py_builtins.isinstance(obj, object)) && (typeof obj.PY$__float__ != 'undefined')) {
         return obj.PY$__float__();
     } else {
-        return __float_real__(obj);
+        return __float_real__(cls, obj);
     }
 };
 
