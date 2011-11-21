18d4d184ba3aa0a453436ccd3d9c8be891123201
Christian Iversen <ci@sikkerhed.org>
2011-11-21 01:15:45 +0100

    * Set the numberclass of floats to "float". Doh.

--- library/29-type-float.js
+++ library/29-type-float.js
@@ -28,7 +28,7 @@ var float = __inherit(number, "float");
 $PY.float = float;
 
 float.numbertype = "PY$__float__";
-float.numberclass = int;
+float.numberclass = float;
 
 float.PY$__init__ = function(value) {
     var s = value.toString();
