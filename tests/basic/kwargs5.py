ec227599de94df6b9786214a4d6899f2378c16a3
Christian Iversen <ci@sikkerhed.org>
2011-11-28 23:09:13 +0100

    + Added tests/basic/kwargs5.py

---
+++ tests/basic/kwargs5.py
@@ -0,0 +1,11 @@
+
+def foo(a, b, c, d = 10):
+    print a, b, c, d
+
+foo(1, 2, 3, 4)
+foo(1, 2, 3)
+foo(1, 2, 3, d = 20)
+foo(1, 2, c = 10, d = 20)
+foo(d = 4, c = 3, b = 2, a = 1)
+foo(**dict(d = 4, c = 3, b = 2, a = 1))
+
