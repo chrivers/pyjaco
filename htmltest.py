171b49fc6f405321f23b29c19221e36377be41ff
Christian Iversen <ci@sikkerhed.org>
2011-12-02 23:13:31 +0100

    * Improved htmltest generator

--- htmltest.py
+++ htmltest.py
@@ -10,17 +10,16 @@ template = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.o
   <script src="py-builtins.js" type="text/javascript"></script>
   <script src="jquery-1.6.1.js" type="text/javascript"></script>
 </head>
-<style>
+<style type="text/css">
   body { font-family: monospace; }
   table { width: 40em; margin: 0 auto; background-color: #EEE; border-radius: 1em; padding: 1em; }
-  
 </style>
 <script type="text/javascript">
   $(document).ready(function() {
     out = ""
     py_builtins.print = function() {
       var args = tuple(Array.prototype.slice.call(arguments));
-      out += str(" ").PY$join(args);
+      out += js(str(" ").PY$join(args));
       out += "\\n";
     };
 
@@ -29,30 +28,28 @@ template = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.o
     %(tests)s;
     %(res)s;
 
-    for (var i in tests) {
+//    var X = function() { return 42; }; (function() { var X = X(); console.log(X); })();
+
+    // COUNTER_var_ is named funny because it must not collide with
+    // the namespace of any test case
+    for (var COUNTER_var_ in tests) {
       out = "";
       try {
-        eval("(function() {" + tests[i] + "})();");
-        if (out == res[i]) {
-          $("#" + i).text("OK").css('background-color', 'green');
+        eval("(function() {\\n" + tests[COUNTER_var_] + "})();");
+        if (out == res[COUNTER_var_]) {
+          $("#" + COUNTER_var_).text("OK").css('background-color', 'green');
         } else {
+          $("#" + COUNTER_var_).text("FAIL").css('background-color', 'red');
           console.log(tests[i]);
           console.log(res[i]);
           console.log(out)
-          $("#" + i).text("FAIL").css('background-color', 'red');
         }
       } catch (e) {
-        $("#" + i).text("CRASH").css('background-color', 'purple');
+        $("#" + COUNTER_var_).text("CRASH").css('background-color', 'purple');
         console.log(e);
       }
     }
-/*
-    if (out == exp) {
-      $("#status-basic-sqrt-py").text("OK").css('background-color', 'green');
-    } else {
-      $("#status-basic-sqrt-py").text("FAIL").css('background-color', 'red');
-    }
-*/
+
   });
 </script>
 <body>
@@ -71,17 +68,12 @@ rows = []
 import glob
 
 def encode(s):
-    #.encode('string_escape').replace("\"", "\\\"")
-    #.encode('string_escape').replace("\"", "\\\"").replace("\\\\", "\\\\\\\\").replace("\n", "\\\n")
-#    return s.encode('string_escape').replace('\"', '\\\"')
     return s.replace("\\", "\\\\").replace("\n", "\\n").replace('\"', '\\\"').replace('\'', '\\\'')
 
 for x in glob.glob("tests/**/*.py"):
     if not (os.path.exists("%s.js" % x) and os.path.exists("%s.out" % x)):
         continue
     name = x.replace("/", "-").replace(".", "-").replace("_", "-")
-#    js.append('tests["%s"] = "%s"' % (name, file("%s.js" % x).read()[24:].encode('base64').replace("\n", "")))
-#    res.append('res["%s"] = "%s"' % (name, file("%s.out" % x).read().encode('base64').replace("\n", "")))
     js.append('tests["%s"] = "%s"' % (name, encode(file("%s.js" % x).read()[24:])))
     res.append('res["%s"] = "%s"' % (name, encode(file("%s.out" % x).read())))
     rows.append("<tr><td>%s</td><td id='%s'>Not run</td></tr>" % (name, name))
