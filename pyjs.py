5e8dcd9f0091435ea665ed1eb8347bcb6dc28898
Christian Iversen <ci@sikkerhed.org>
2011-12-04 15:45:48 +0100

    * Make pyjaco compiler look for /usr/share/pyjaco/py-builtins.js    so it works when installed system-wide using the debian package  + Added -N option for not including the standard library  + Added check that either -I, -i or -N is specified  + Added comments when generating files with -i, so it is easier to    tell where the included code ends, and the user code begins.

--- pyjs.py
+++ pyjs.py
@@ -5,29 +5,43 @@ import os.path
 from optparse import OptionParser
 from pyjaco import Compiler
 
+if os.path.exists("py-builtins.js"):
+    path_library = "py-builtins.js"
+else:
+    path_library = "/usr/share/pyjaco/py-builtins.js"
+
 def main():
     parser = OptionParser(usage="%prog [options] filename",
                           description="Python to JavaScript compiler.")
 
     parser.add_option("--output",
-                      action="store",
-                      dest="output",
-                      help="write output to OUTPUT")
+                      action = "store",
+                      dest   = "output",
+                      help   = "write output to OUTPUT")
 
     parser.add_option("-i", "--include-builtins",
-                      action="store_true",
-                      dest="include_builtins",
-                      default=False,
-                      help="include py-builtins.js library in the output")
+                      action  = "store_true",
+                      dest    = "include_builtins",
+                      default = False,
+                      help    = "include py-builtins.js library in the output")
 
     parser.add_option("-I", "--import-builtins",
-                      action="store_true",
-                      dest="import_builtins",
-                      default=False,
-                      help="call load('py-builtins.js') to source the standard library")
+                      action  = "store_true",
+                      dest    = "import_builtins",
+                      default = False,
+                      help    = "call load('py-builtins.js') to source the standard library")
+
+    parser.add_option("-N", "--no-include",
+                      action  = "store_true",
+                      dest    = "no_builtins",
+                      default = False,
+                      help    = "Do not include attempt to load py-builtins.js. The generated javascript code will not run by itself, unless this library is included")
 
     options, args = parser.parse_args()
-    if len(args) == 1:
+
+    if options.include_builtins + options.import_builtins + options.no_builtins <> 1 and len(args) == 1:
+        print "You must specify either the -i, -I or -N mode!"
+    elif len(args) == 1:
         filename = args[0]
 
         if options.output:
@@ -36,13 +50,12 @@ def main():
             output = sys.stdout
 
         if options.include_builtins:
-            if os.path.dirname(__file__):
-                builtins = open(os.path.join(os.path.dirname(__file__), "py-builtins.js")).read()
-            else:
-                builtins = open("py-builtins.js").read()
+            builtins = open(path_library).read()
+            output.write("  Standard library  ".center(80, "#") + "\n")
             output.write(builtins)
+            output.write("  User code  ".center(80, "#") + "\n")
         elif options.import_builtins:
-            output.write('load("py-builtins.js");\n')
+            output.write('load("%s");\n' % path_library)
 
         c = Compiler()
         c.append_string(open(filename).read())
