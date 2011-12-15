13592f86ccb222e254112627c9ed58ac1d5e11a7
Stanislav Feldman <stanislavfeldman@gmail.com>
2011-12-15 11:21:49 +0400

    add setup.py for easy_install and pip and change gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+try:
+	from setuptools import setup
+except:
+	pass
+
+setup(
+    name = "pyjaco",
+    version = "1.0.0",
+    author = "Mateusz Paprocki",
+    author_email = "mattpap@gmail.com",
+    description = ("Python to JavaScript translator"),
+    url = "http://pyjaco.org",
+    keywords = "javascript translator compiler",
+    packages=["pyjaco", "pyjaco.compiler"],
+)
