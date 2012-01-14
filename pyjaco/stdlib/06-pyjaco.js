757c6df8c00ae873ea3e506c0a0798e84616e3a2
Christian Iversen <ci@sikkerhed.org>
2012-01-14 16:42:58 +0100

    + Added 06-pyjaco.js with all the collection $PY.* functions

---
+++ pyjaco/stdlib/06-pyjaco.js
@@ -0,0 +1,92 @@
+/**
+  Copyright 2011-2012 Christian Iversen <ci@sikkerhed.org>
+
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation
+  files (the "Software"), to deal in the Software without
+  restriction, including without limitation the rights to use,
+  copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following
+  conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
+**/
+
+$PY.isinstance = function(obj, cls) {
+    if (cls instanceof Array) {
+        for (var i = 0; i < cls.length; i++) {
+            var c = obj.PY$__class__;
+            while (c) {
+                if (c === cls[i])
+                    return true;
+                c = c.PY$__super__;
+            }
+        }
+
+        return false;
+    } else {
+        var c = obj.PY$__class__;
+        while (c) {
+            if (c === cls)
+                return true;
+            c = c.PY$__super__;
+        }
+        return false;
+    }
+};
+
+$PY.repr = function(obj) {
+    return __builtins__.PY$repr(obj)._js_();
+};
+
+$PY.len = function(obj) {
+    var c = obj.PY$__class__;
+    if (c === list || c === tuple || c === str || c === basestring || c === unicode) {
+        return obj.obj.length;
+    } else if (obj.PY$__len__ !== undefined) {
+        return obj.PY$__len__()._js_();
+    } else {
+        throw __builtins__.PY$AttributeError('__len__');
+    }
+};
+
+$PY.next = function(obj) {
+    if (obj.PY$__class__ === iter) {
+        return obj.next();
+    } else {
+        try {
+            return obj.PY$next();
+        } catch (x) {
+            if ($PY.isinstance(x, __builtins__.PY$StopIteration)) {
+                return null;
+            } else {
+                throw x;
+            }
+       }
+    }
+};
+
+$PY.__not__ = function(obj) {
+   if (obj.PY$__nonzero__ !== undefined) {
+       return js(obj.PY$__nonzero__()) ? False : True;
+   } else if (obj.PY$__len__ !== undefined) {
+       return js(obj.PY$__len__()) === 0 ? True : False;
+   } else {
+       return js(obj) ? False : True;
+   }
+};
+
+$PY.__is__ = function(a, b) {
+    return a === b ? True : False;
+};
