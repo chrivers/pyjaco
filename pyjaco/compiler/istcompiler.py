b8976b2aa88622e7503535595f6756cac7a4ff45
Christian Iversen <ci@sikkerhed.org>
2012-01-17 12:16:54 +0100

    + Implemented rudimentary compilation steps for attribute, boolop,    call, expr, if, module, num and return.  * Fixed self.bi value; should be ist.Name, not str

--- pyjaco/compiler/istcompiler.py
+++ pyjaco/compiler/istcompiler.py
@@ -30,28 +30,57 @@ from ist import *
 
 class Compiler(object):
 
-    bi = "__builtins__"
+    bi = Name(id = "__builtins__")
 
     def __init__(self):
         pass
 
     def compile(self, source, filename = "<unknown>"):
         code = ast.parse(source, filename)
-        
+
         return self.comp(code)
 
     def comp(self, node):
         name = "comp_%s" % node.__class__.__name__.lower()
+#        print node.__class__.__name__, [x for x in dir(node) if not x.startswith("_")]
         if hasattr(self, name):
             return getattr(self, name)(node)
         else:
             raise NotImplementedError("IST Compiler does not support node type [%s]" % node.__class__.__name__)
 
-    def comp_module(self, node):
-        return Block(body = [self.comp(n) for n in node.body])
+    ## Multiplexed functions
+
+    def comp_attribute(self, node):
+        return GetAttr(base = self.comp(node.value), attr = node.attr)
+
+    def comp_binop(self, node):
+        return BinOp(left = self.comp(node.left), right = self.comp(node.right), op = node.op.__class__.__name__)
+
+    def comp_boolop(self, node):
+        return BinOp(left = self.comp(node.values[0]), right = self.comp(node.values[1]), op = node.op.__class__.__name__)
+
+    def comp_call(self, node):
+        assert node.kwargs == None
+        assert node.starargs == None
+        return Call(func = self.comp(node.func), params = [self.comp(n) for n in node.args])
+
+    def comp_expr(self, node):
+        return self.comp(node.value)
 
     def comp_functiondef(self, node):
         return Function(body = [self.comp(n) for n in node.body], name = node.name, params = [])
+
+    def comp_if(self, node):
+        return If(cond = self.comp(node.test), body = [self.comp(n) for n in node.body], orelse = [self.comp(n) for n in node.orelse])
+
+    def comp_module(self, node):
+        return Block(body = [self.comp(n) for n in node.body])
+
+    def comp_name(self, node):
+        return Name(id = node.id)
+
+    def comp_num(self, node):
+        return Value(value = node.n)
 
     def comp_print(self, node):
         assert node.dest == None
@@ -59,8 +88,5 @@ class Compiler(object):
 
         return Call(func = GetAttr(base = self.bi, attr = "print"), params = [self.comp(n) for n in node.values])
 
-    def comp_binop(self, node):
-        return BinOp(left = self.comp(node.left), right = self.comp(node.right), op = node.op.__class__.__name__)
-
-    def comp_name(self, node):
-        return Name(id = node.id)
+    def comp_return(self, node):
+        return Return(expr = self.comp(node.value))
