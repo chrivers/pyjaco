a68c2827cdd3efebaae36ce92f544124419593b5
Christian Iversen <ci@sikkerhed.org>
2012-05-06 23:17:22 +0200

    + Added more syntax testing for newpyjaco

--- newpyjaco.py
+++ newpyjaco.py
@@ -6,5 +6,8 @@ c = pyjaco.compiler.istcompiler.Compiler()
 
 def foo(x, y, z):
     print x + y * z
+    if True and False:
+        foo.bar.baz(17)
+    return 42
 
 print c.compile(inspect.getsource(foo)).str()
