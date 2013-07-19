8233f9819f13e0383a8ed7c391d90965de95f2e8
Christian Iversen <ci@sikkerhed.org>
2013-07-19 20:21:14 +0200

    + Added base multiplexer class  * Split Compiler into Compiler and ISTCompiler to preserve single responsibility

--- pyjaco/compiler/istcompiler.py
+++ pyjaco/compiler/istcompiler.py
@@ -1,6 +1,6 @@
 ######################################################################
 ##
-## Copyright 2012 Christian Iversen <ci@sikkerhed.org>
+## Copyright 2013 Christian Iversen <ci@sikkerhed.org>
 ##
 ## Permission is hereby granted, free of charge, to any person
 ## obtaining a copy of this software and associated documentation
@@ -30,63 +30,116 @@ from ist import *
 
 class Compiler(object):
 
-    bi = Name(id = "__builtins__")
-
     def __init__(self):
-        pass
+        self.compiler = ISTCompiler()
 
     def compile(self, source, filename = "<unknown>"):
         code = ast.parse(source, filename)
 
-        return self.comp(code)
+        return self.compiler.comp(code)
+
+class Multiplexer(object):
 
     def comp(self, node):
-        name = "comp_%s" % node.__class__.__name__.lower()
+        name = "node_%s" % node.__class__.__name__.lower()
 #        print node.__class__.__name__, [x for x in dir(node) if not x.startswith("_")]
         if hasattr(self, name):
             return getattr(self, name)(node)
         else:
-            raise NotImplementedError("IST Compiler does not support node type [%s]" % node.__class__.__name__)
+            raise NotImplementedError("%s does not support node type [%s]" % (self.__class__.__name__, node.__class__.__name__))
 
     ## Multiplexed functions
 
-    def comp_attribute(self, node):
+    def node_block(self, node):
+        raise NotImplementedError()
+
+    def node_attribute(self, node):
+        raise NotImplementedError()
+
+    def node_binop(self, node):
+        raise NotImplementedError()
+
+    def node_boolop(self, node):
+        raise NotImplementedError()
+
+    def node_call(self, node):
+        raise NotImplementedError()
+
+    def node_expr(self, node):
+        raise NotImplementedError()
+
+    def node_functiondef(self, node):
+        raise NotImplementedError()
+
+    def node_if(self, node):
+        raise NotImplementedError()
+
+    def node_module(self, node):
+        raise NotImplementedError()
+
+    def node_name(self, node):
+        raise NotImplementedError()
+
+    def node_num(self, node):
+        raise NotImplementedError()
+
+    def node_print(self, node):
+        raise NotImplementedError()
+
+    def node_return(self, node):
+        raise NotImplementedError()
+
+    def node_str(self, node):
+        raise NotImplementedError()
+
+class ISTCompiler(Multiplexer):
+    bi = Name(id = "__builtins__")
+
+    ## Multiplexed functions
+
+    def node_attribute(self, node):
         return GetAttr(base = self.comp(node.value), attr = node.attr)
 
-    def comp_binop(self, node):
+    def node_binop(self, node):
         return BinOp(left = self.comp(node.left), right = self.comp(node.right), op = node.op.__class__.__name__)
 
-    def comp_boolop(self, node):
+    def node_boolop(self, node):
         return BinOp(left = self.comp(node.values[0]), right = self.comp(node.values[1]), op = node.op.__class__.__name__)
 
-    def comp_call(self, node):
+    def node_call(self, node):
         assert node.kwargs == None
         assert node.starargs == None
         return Call(func = self.comp(node.func), params = [self.comp(n) for n in node.args])
 
-    def comp_expr(self, node):
+    def node_expr(self, node):
         return self.comp(node.value)
 
-    def comp_functiondef(self, node):
+    def node_functiondef(self, node):
         return Function(body = [self.comp(n) for n in node.body], name = node.name, params = [])
 
-    def comp_if(self, node):
+    def node_if(self, node):
         return If(cond = self.comp(node.test), body = [self.comp(n) for n in node.body], orelse = [self.comp(n) for n in node.orelse])
 
-    def comp_module(self, node):
+    def node_module(self, node):
         return Block(body = [self.comp(n) for n in node.body])
 
-    def comp_name(self, node):
+    def node_name(self, node):
         return Name(id = node.id)
 
-    def comp_num(self, node):
+    def node_num(self, node):
         return Value(value = node.n)
 
-    def comp_print(self, node):
+    def node_print(self, node):
         assert node.dest == None
         assert node.nl
 
         return Call(func = GetAttr(base = self.bi, attr = "print"), params = [self.comp(n) for n in node.values])
 
-    def comp_return(self, node):
+    def node_return(self, node):
         return Return(expr = self.comp(node.value))
+
+    def node_str(self, node):
+        return String(value = node.s)
+
+    def node_pass(self, node):
+        return Block(body = [])
