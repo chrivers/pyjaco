308e00b4f002a7480397b148d658570edbe29936
Christian Iversen <ci@sikkerhed.org>
2013-07-20 13:53:11 +0200

    * Generalized isttest.py

--- pyjaco/compiler/isttest.py
+++ pyjaco/compiler/isttest.py
@@ -1,6 +1,11 @@
 import istcompiler
 import istprinter
+import sys
+
+if len(sys.argv) == 1:
+    print "Usage: %s <file.py>" % sys.argv[0]
+    sys.exit()
 
 c = istcompiler.Compiler()
-code = c.compile(file("test.py").read(), "test.py")
+code = c.compile(file(sys.argv[1]).read(), "test.py")
 print istprinter.format(code)
