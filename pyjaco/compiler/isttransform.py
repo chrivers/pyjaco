252b422e1bb77afeb3fd2da703825738c900f8e8
Christian Iversen <ci@sikkerhed.org>
2013-07-24 20:30:28 +0200

    + Added support for destiny function in Transformer. This makes it    possible to look up the tree and see what node types we are coming    from. This has an important impact on many operations

--- pyjaco/compiler/isttransform.py
+++ pyjaco/compiler/isttransform.py
@@ -3,6 +3,17 @@ import istcompiler
 
 class Transformer(istcompiler.Multiplexer):
 
+    def __init__(self):
+        self.stack = []
+        return super(Transformer, self).__init__()
+
+    def destiny(self, names, skip):
+        for name in reversed(self.stack[:-skip]):
+            if name in names:
+                return name
+        else:
+            return False
+
     def comp(self, node):
         if isinstance(node, list):
             res = []
@@ -15,12 +26,16 @@ class Transformer(istcompiler.Multiplexer):
                     res.append(n)
             return res
         else:
-            name = "node_%s" % node.__class__.__name__.lower()
+            classname = node.__class__.__name__.lower()
+            name = "node_%s" % classname
+            self.stack.append(classname)
             if hasattr(self, name):
-                return getattr(self, name)(node)
+                res = getattr(self, name)(node)
             else:
                 for f in node._fields:
                     value = getattr(node, f)
                     if isinstance(value, (ist.ISTNode, list)):
                         setattr(node, f, self.comp(value))
-                return node
+                res = node
+            self.stack.pop()
+            return res
