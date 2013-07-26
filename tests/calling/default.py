e91ac6da2a2e2cd8136eae51599b3bf0780e5870
Christian Iversen <ci@sikkerhed.org>
2013-07-26 15:08:10 +0200

    * Extended identifier names to more than 1 char to catch corner cases

--- tests/calling/default.py
+++ tests/calling/default.py
@@ -1,10 +1,10 @@
 g = 99
 
-def f(a, b=1, c="default c", d=g):
-    print a
-    print b
-    print c
-    print d
+def f(aaa, bbb=1, ccc="default c", ddd=g):
+    print aaa
+    print bbb
+    print ccc
+    print ddd
 
 f(0)
 f(0, 77)
