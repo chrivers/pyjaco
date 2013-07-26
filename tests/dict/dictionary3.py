65405f61f49b8c73391f2f17fa374be660a9f290
Christian Iversen <ci@sikkerhed.org>
2013-07-26 15:09:27 +0200

    * Updated the dictionary3 test to be independent of CPython implementation details

--- tests/dict/dictionary3.py
+++ tests/dict/dictionary3.py
@@ -2,12 +2,12 @@ x = dict()
 x[1] = "1"
 x["1"] = "2"
 
-print x
+print ", ".join(sorted(["%r: %r" % item for item in x.items()]))
 
 y = dict(x)
 y[True] = "true"
 y[False] = "false"
 
-print y
+print ", ".join(sorted(["%r: %r" % item for item in y.items()]))
 
 print y[1], y[0]
