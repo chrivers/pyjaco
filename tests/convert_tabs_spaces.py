eaeda3f323e6107aa1401e4605af248d7f218083
Christian Iversen <ci@sikkerhed.org>
2013-07-26 16:15:51 +0200

    - Removed obsolete script

--- tests/convert_tabs_spaces.py
+++
@@ -1,7 +0,0 @@
-from glob import glob
-files = glob("*.py")
-for file in files:
-    print "Converting ", file
-    f = open(file).read()
-    f = f.replace("\t", " "*4)
-    open(file, "w").write(f)
