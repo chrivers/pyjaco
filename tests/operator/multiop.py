7ccc3efe06c9563e82c1d85a0b7e01f01040ea5d
Christian Iversen <ci@sikkerhed.org>
2013-07-28 01:31:39 +0200

    * Fixed speling error in unittest

--- tests/operator/multiop.py
+++ tests/operator/multiop.py
@@ -3,9 +3,9 @@ A = B = C = range(4)
 for a in A:
     for b in B:
         for c in C:
-            print A < B < C
-            print A > B > C
-            print A <= B <= C
-            print A >= B >= C
-            print A < B > C
-            print A > B < C
+            print a < b < c
+            print a > b > c
+            print a <= b <= c
+            print a >= b >= c
+            print a < b > c
+            print a > b < c
