dbe31c06e02c88d792feb3232950c7cff56e8a8f
Christian Iversen <ci@sikkerhed.org>
2013-07-28 01:31:20 +0200

    * Removed 3 known-to-fail tests now that they work

--- testtools/known_to_fail.py
+++ testtools/known_to_fail.py
@@ -1,15 +1,12 @@
 """lists all the tests that are known to fail"""
 KNOWN_TO_FAIL = [
-    "tests/bool/boolop.py",
     "tests/class/getattr2.py",
     "tests/class/oo_diamond.py",
     "tests/class/oo_super.py",
     "tests/namespace/del_global.py",
     "tests/namespace/del_local.py",
-    "tests/list/listcomp2.py",
     "tests/list/assign_slice_step.py",
     "tests/functions/cmp.py",
-    "tests/operator/multiop-sideeffect.py",
     "tests/operator/type-comparison.py",
     "tests/flow/yield.py",
 
