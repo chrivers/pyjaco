e7c7336836ca4b0a7c28296b935df6949b6567df
Christian Iversen <ci@sikkerhed.org>
2013-07-28 03:43:06 +0200

    * Modified type-comparison test to disregard implementation    details. It turns out that the cmp(type1, type2) is something the    different python implementations (cpython, pypy, pyjaco) seriously    disagree on, and there is no "right way" since it's left up to the    implementers to decide on an ordering, as long as it is    consistent. Instead, the test case now tests equality testing and    that the comparison result is in (-1, 0, 1). This did reveal some    bugs, which are now fixed.

--- tests/operator/type-comparison.py
+++ tests/operator/type-comparison.py
@@ -2,4 +2,4 @@ L = [str, int, unicode, bool, list, tuple, dict]
 
 for x in L:
     for y in L:
-        print "%s vs %s: %s" % (x.__name__, y.__name__, cmp(x, y))
+        print "%s == %s: %s (cmp %s)" % (x.__name__, y.__name__, x == y, cmp(x, y) in (-1, 0, 1))
