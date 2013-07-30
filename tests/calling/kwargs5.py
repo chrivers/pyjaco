c828f8e7c52bc569bf916e2e35abf2df870a04a6
Christian Iversen <ci@sikkerhed.org>
2013-07-30 21:15:58 +0200

    * Made kwargs5 cover more cases

--- tests/calling/kwargs5.py
+++ tests/calling/kwargs5.py
@@ -7,5 +7,12 @@ foo(1, 2, 3)
 foo(1, 2, 3, d = 20)
 foo(1, 2, c = 10, d = 20)
 foo(d = 4, c = 3, b = 2, a = 1)
-foo(**dict(d = 4, c = 3, b = 2, a = 1))
 
+foo(**dict(d = 4, c = 3, b = 2, a = 1))
+foo(d = 4, **dict(c = 3, b = 2, a = 1))
+foo(d = 4, c = 3, **dict(b = 2, a = 1))
+foo(d = 4, c = 3, b = 2, **dict(a = 1))
+foo(d = 4, c = 3, b = 2, a = 1, **dict())
+
+d = dict(d = 4, c = 3, b = 2, a = 1)
+foo(**d)
