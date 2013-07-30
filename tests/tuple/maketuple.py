784d1164c357f2174580e72041a2a522c7a600cc
Christian Iversen <ci@sikkerhed.org>
2013-07-30 22:22:47 +0200

    + Added test to show various ways to construct a tuple

---
+++ tests/tuple/maketuple.py
@@ -0,0 +1,14 @@
+print tuple()
+print tuple([1])
+print tuple([1, 2])
+print tuple([1, 2, 3])
+
+print tuple(())
+print tuple((1,))
+print tuple((1, 2))
+print tuple((1, 2, 3))
+
+print tuple(())
+print tuple(("a",))
+print tuple(("a", "b"))
+print tuple(("a", "b", "c"))
