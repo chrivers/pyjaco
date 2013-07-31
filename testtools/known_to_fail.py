df1c975834849f8730c38539ea827f97d0e99f27
Christian Iversen <ci@sikkerhed.org>
2013-07-31 23:57:13 +0200

    - Removed getattr2 test, since it works now. Added oo_decorated and too-few-args to known-to-fail

--- testtools/known_to_fail.py
+++ testtools/known_to_fail.py
@@ -1,8 +1,9 @@
 """lists all the tests that are known to fail"""
 KNOWN_TO_FAIL = [
-    "tests/class/getattr2.py",
+    "tests/calling/too-few-args.py",
     "tests/class/oo_diamond.py",
     "tests/class/oo_super.py",
+    "tests/class/oo_decorated.py",
     "tests/namespace/del_global.py",
     "tests/namespace/del_local.py",
     "tests/list/assign_slice_step.py",
