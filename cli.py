2cc51730568a4c2cee11e27f4aa1f841f74f5bc6
Christian Iversen <ci@sikkerhed.org>
2013-08-02 23:00:41 +0200

    + Added the first version of the CLI program. It has a very hackish    interface to the javascript executable, but it works well enough    for simple tests. Now pyjaco really feels like python!

---
+++ cli.py
@@ -0,0 +1,103 @@
+import subprocess
+import pyjaco
+import ast
+import select
+from pyjaco import Compiler
+import platform
+import sys
+
+debug = len(sys.argv) > 1 and sys.argv[1] == '-d'
+
+compiler = Compiler()
+
+def pyjaco_compile(code, printoutput):
+    if printoutput:
+        return "try {print($PY.repr(%s), '\\n')} finally {print('\\nMARK\\n')};\n" % "\n".join(compiler._compile(code).split("\n")[1:])[:-1]
+    else:
+        return "try {%s} finally {print('\\nMARK\\n')};\n" % "".join(compiler._compile(code).split("\n")[1:])
+
+proc = subprocess.Popen("js", stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+
+proc.stdin.write("load('py-builtins.js');\n")
+proc.stdin.write("print('MARK');\n");
+while True:
+    line = proc.stdout.readline()
+    if line.endswith("MARK\n"):
+        break
+
+print "Python 2.x (pyjaco)"
+print "[google v8] on %s" % platform.system()
+print 'Type "help", "copyright", "credits" or "license" for more information.'
+
+while True:
+    try:
+        lastline = raw_input(">>> ").rstrip()
+        if lastline:
+            code = lastline
+            while True:
+                tree = None
+                try:
+                    tree = ast.parse(code)
+                except SyntaxError, E:
+                    if not E.msg.startswith("unexpected EOF"):
+                        print "SyntaxError in code: %s" % E.message
+
+                linetype = type(tree.body[0]) if tree and tree.body else None
+
+                if linetype in (ast.Expr, ast.Print, ast.Assign) or lastline == "":
+                    break
+                else:
+                    while True:
+                        lastline = raw_input("... ").rstrip()
+                        code += "\n" + lastline
+                        try:
+                            tree = ast.parse(code)
+                        except SyntaxError, E:
+                            if not E.msg.startswith("unexpected EOF"):
+                                print "SyntaxError in code: %s" % E.message
+                        else:
+                            if lastline == "":
+                                break
+
+            jscode = pyjaco_compile(code, printoutput = linetype in (ast.Expr, ))
+            if debug:
+                print "[v8 input] >>>%s<<<" % jscode.rstrip()
+            proc.stdin.write(jscode)
+
+            while True:
+                output = proc.stdout.readline()
+                if debug:
+                    print "-[v8 reply] >>>%r<<<" % output
+                if output.startswith("d8> "):
+                    output = output[4:]
+
+                if output == "\n":
+                    continue
+                # Some versions of the v8 shell produce spurious (but harmless) "undefined" messages
+                elif output == "undefined\n":
+                    continue
+                elif output == "MARK\n":
+                    proc.stdin.write("\nprint('\\nSENTINEL\\n');\n");
+                    while True:
+                        output = proc.stdout.readline()
+                        if output.startswith("d8> "):
+                            output = output[4:]
+
+                        if debug:
+                            print "--[v8 reply] >>>%r<<<" % output
+                        if output == "SENTINEL\n":
+                            break
+                        # Some versions of the v8 shell produce spurious (but harmless) "undefined" messages
+                        elif output == "undefined\n":
+                            continue
+                        elif output == "\n":
+                            continue
+                        else:
+                            print "Error:", output,
+                    break
+                elif linetype in (ast.Expr, ast.Print) and output.rstrip(" \n") <> "None":
+                    print output,
+
+    except EOFError:
+        print
+        break
