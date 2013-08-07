544daac03dc71d200821f08fd8ba3f81a3f9e44e
Christian Iversen <ci@sikkerhed.org>
2013-08-07 13:38:42 +0200

    * Changed iter/generator test to avoid using parameter names    (min/max) that can trigger a namespace bug, since that is unrelated    to generator semantics.

--- tests/iter/generator.py
+++ tests/iter/generator.py
@@ -4,18 +4,18 @@ class generator:
     class iterator:
         def __init__(self,parent):
             self.parent = parent
-            self.value = parent.min
+            self.value = parent.low
 
         def next(self):
             val = self.value
-            if val > self.parent.max:
+            if val > self.parent.high:
                 raise StopIteration
             self.value += 1
             return val
 
-    def __init__(self,min,max):
-        self.min = min
-        self.max = max
+    def __init__(self, low, high):
+        self.low = low
+        self.high = high
 
     def __iter__(self):
         return self.iterator(self)
