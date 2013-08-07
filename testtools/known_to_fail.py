9e62ba1bcfc5cccef993e9fc021febc873a6db24
Christian Iversen <ci@sikkerhed.org>
2013-08-07 14:34:16 +0200

    * Updated known-to-fail

--- testtools/known_to_fail.py
+++ testtools/known_to_fail.py
@@ -1,16 +1,15 @@
 """lists all the tests that are known to fail"""
 KNOWN_TO_FAIL = [
     "tests/calling/too-few-args.py",
+    "tests/calling/functiontype.py",
     "tests/class/oo_diamond.py",
     "tests/class/oo_super.py",
-    "tests/class/oo_decorated.py",
     "tests/dict/customhash.py",
     "tests/namespace/del_global.py",
     "tests/namespace/del_local.py",
     "tests/list/assign_slice_step.py",
     "tests/functions/cmp.py",
     "tests/flow/yield.py",
-    "tests/iter/generator.py",
 
     "tests/libraries/xmlwriter.py",
     "tests/modules/classname.py",
