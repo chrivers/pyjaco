64213de6dab9462db60f60be32044965450de6de
Christian Iversen <ci@sikkerhed.org>
2013-08-11 17:34:08 +0200

    * Updated assign-slice tests

--- tests/list/assign_slice.py
+++ tests/list/assign_slice.py
@@ -1,20 +1,14 @@
-
-a = [1,2,3,4,5,6,7]
-
-a[4:6] = ['a','b']
-
 def show(x):
     print "----"
     for x in a:
         print x
 
+a = [1,2,3,4,5,6,7]
+a[4:6] = ['a','b']
 show(a)
 
-
 a[2:4] = ['z']
-
 show(a)
 
 a[0:2] = ['abc','def','abc','def']
-
 show(a)
--- tests/list/assign_slice_step.py
+++ tests/list/assign_slice_step.py
@@ -4,5 +4,10 @@ print x
 x[2:10:2] = ["a", "b", "c", "d"]
 print x
 
-y = range(10)
-x[2:10:3] = ["a", "b", "c", "d"]
+x = range(10)
+x[2:5:1] = ["a", "b", "c", "d"]
+print x
+
+x = range(10)
+x[5:2:-1] = ["a", "b", "c"]
+print x
