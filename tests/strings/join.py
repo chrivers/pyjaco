8ad793dce34ab7aed3b973aae729d6a943a2381c
Christian Iversen <ci@sikkerhed.org>
2013-08-11 19:49:38 +0200

    * Extended strings/join.py

--- tests/strings/join.py
+++ tests/strings/join.py
@@ -3,3 +3,9 @@ print "".join(a)
 print " ".join(a)
 print "x".join(a)
 print "x ".join(a)
+
+b = ["", "", "foo", "x", "", ""]
+print "".join(b)
+print " ".join(b)
+print "x".join(b)
+print " x ".join(b)
