2cebe2611359e9789e1947407f7990875da0d3ce
Christian Iversen <ci@sikkerhed.org>
2013-08-11 14:22:23 +0200

    * Cleaned up CLI code, and allow the CLI program to run files directly

--- cli.py
+++ cli.py
@@ -7,40 +7,53 @@ from pyjaco import Compiler
 import platform
 import sys, os
 import readline
+from optparse import OptionParser
 
-debug = len(sys.argv) > 1 and sys.argv[1] == '-d'
+def parse_args():
+    parser = OptionParser(usage="""%prog [options] [<file.py>]""",
+                          description="Pyjaco command-line interface.")
 
-compiler = Compiler()
+    parser.add_option("-d", "--debug",
+                      action = "store_true",
+                      dest   = "debug",
+                      help   = "Debug mode. Show all communication with the JS interpreter.")
 
-def pyjaco_compile(code, printoutput):
+    return parser.parse_args()
+
+def pyjaco_compile(compiler, code, printoutput):
     if printoutput:
         return "try {print($PY.repr(%s), '\\n')} finally {print('\\nMARK\\n')};\n" % "\n".join(compiler._compile(code).split("\n")[1:])[:-1]
     else:
         return "try {%s} finally {print('\\nMARK\\n')};\n" % "".join(compiler._compile(code).split("\n")[1:])
 
-if not os.path.exists("py-builtins.js"):
-    print "Standard library (py-builtins.js) not found. Please 'make stdlib'"
-    sys.exit(1)
+def start_checks():
+    if not os.path.exists("py-builtins.js"):
+        print "Standard library (py-builtins.js) not found. Please 'make stdlib'"
+        sys.exit(1)
 
-try:
-    proc = subprocess.Popen("js", stdin = subprocess.PIPE, stdout = subprocess.PIPE)
-except OSError, E:
-    print "Could not execute 'js' javascript engine: %s" % E
-    sys.exit(1)
-
-proc.stdin.write("load('py-builtins.js');\n")
-proc.stdin.write("print('MARK');\n");
-while True:
-    line = proc.stdout.readline()
-    if line.endswith("MARK\n"):
-        break
-
-print "Python 2.x (pyjaco)"
-print "[google v8] on %s" % platform.system()
-print 'Type "help", "copyright", "credits" or "license" for more information.'
-
-while True:
+def start_js():
     try:
+        proc = subprocess.Popen("js", stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+    except OSError, E:
+        print "Could not execute 'js' javascript engine: %s" % E
+        sys.exit(1)
+
+    proc.stdin.write("load('py-builtins.js');\n")
+    proc.stdin.write("print('MARK');\n");
+    while True:
+        line = proc.stdout.readline()
+        if line.endswith("MARK\n"):
+            break
+
+    return proc
+
+def print_banner():
+    print "Python 2.x (pyjaco)"
+    print "[google v8] on %s" % platform.system()
+    print 'Type "help", "copyright", "credits" or "license" for more information.'
+
+def interactive_read():
+    while True:
         lastline = raw_input(">>> ").rstrip()
         if lastline:
             code = lastline
@@ -68,46 +81,63 @@ while True:
                         else:
                             if lastline == "":
                                 break
+            break
 
-            jscode = pyjaco_compile(code, printoutput = linetype in (ast.Expr, ))
-            if debug:
-                print "[v8 input] >>>%s<<<" % jscode.rstrip()
-            proc.stdin.write(jscode)
+    return code, linetype
 
-            while True:
-                output = proc.stdout.readline()
-                if debug:
-                    print "-[v8 reply] >>>%r<<<" % output
-                if output.startswith("d8> "):
-                    output = output[4:]
+def run_code(compiler, options, code, linetype):
+        jscode = pyjaco_compile(compiler, code, printoutput = linetype in (ast.Expr, ))
+        if options.debug:
+            print "[v8 input] >>>%s<<<" % jscode.rstrip()
+        proc.stdin.write(jscode)
 
-                if output == "\n":
-                    continue
-                # Some versions of the v8 shell produce spurious (but harmless) "undefined" messages
-                elif output == "undefined\n":
-                    continue
-                elif output == "MARK\n":
-                    proc.stdin.write("\nprint('\\nSENTINEL\\n');\n");
-                    while True:
-                        output = proc.stdout.readline()
-                        if output.startswith("d8> "):
-                            output = output[4:]
+        while True:
+            output = proc.stdout.readline()
+            if options.debug:
+                print "-[v8 reply] >>>%r<<<" % output
+            if output.startswith("d8> "):
+                output = output[4:]
 
-                        if debug:
-                            print "--[v8 reply] >>>%r<<<" % output
-                        if output == "SENTINEL\n":
-                            break
-                        # Some versions of the v8 shell produce spurious (but harmless) "undefined" messages
-                        elif output == "undefined\n":
-                            continue
-                        elif output == "\n":
-                            continue
-                        else:
-                            print "Error:", output,
-                    break
-                elif linetype in (ast.Expr, ast.Print) and output.rstrip(" \n") <> "None":
-                    print output,
+            if output == "\n":
+                continue
+            # Some versions of the v8 shell produce spurious (but harmless) "undefined" messages
+            elif output == "undefined\n":
+                continue
+            elif output == "MARK\n":
+                proc.stdin.write("\nprint('\\nSENTINEL\\n');\n");
+                while True:
+                    output = proc.stdout.readline()
+                    if output.startswith("d8> "):
+                        output = output[4:]
 
-    except EOFError:
-        print
-        break
+                    if options.debug:
+                        print "--[v8 reply] >>>%r<<<" % output
+                    if output == "SENTINEL\n":
+                        break
+                    # Some versions of the v8 shell produce spurious (but harmless) "undefined" messages
+                    elif output == "undefined\n":
+                        continue
+                    elif output == "\n":
+                        continue
+                    else:
+                        print "Error:", output,
+                break
+            elif linetype in (ast.Expr, ast.Print) and output.rstrip(" \n") <> "None":
+                print output,
+
+if __name__ == "__main__":
+    start_checks()
+    options, args = parse_args()
+    compiler = Compiler()
+    proc = start_js()
+    if args:
+        run_code(compiler, options, file(args[0]).read(), ast.Print)
+    else:
+        print_banner()
+        while True:
+            try:
+                code, linetype = interactive_read()
+                run_code(compiler, options, code, linetype)
+            except EOFError:
+                print
+                break
